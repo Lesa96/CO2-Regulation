80190a5cfd9487e65ddddfc7a6f2345d5fda364b
Lesa96 <dragomirovic.sasa.96@gmail.com>
2021-11-28 19:42:43 +0100

    Added Service

---
+++ CO2RegulationProject/CO2RegulationProject/Models/PowerPlant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CO2RegulationProject.Models
+{
+    public class PowerPlant
+    {
+        private float _power;
+
+        public float Power
+        {
+            get { return _power; }
+            set { _power = value; }
+        }
+
+    }
+}
